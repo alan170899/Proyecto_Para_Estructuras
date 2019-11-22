3f49a1ee73bd5c930e6810fb45a75b39e9445177
Alan-Franisco <alanfcruzch99@gmail.com>
2019-11-21 22:49:36 -0600

    ELIMINACION DE FUNCION QUE ELIMMINA EL CONTACTO DE ENMMEDIO Y DE BUCAR

--- Agenda.c
+++ Agenda.c
@@ -94,30 +94,6 @@ struct contacto* Eliminar_Contacto_primero(struct contacto *Agenda, char *Nombre
 	}
 	return Agenda;
 }
-struct contacto* Eliminar_Contacto_medio(struct contacto *Agenda, char *Nombre, char *Numero){
-	//Punteros de ayuda
-	struct contacto *Aux1 = NULL;
-	struct contacto *Aux2 = NULL;
-	struct contacto *Aux3 = NULL;
-	
-	if (Agenda != NULL){
-		Aux1 = Agenda;
-		while ((Aux1 != NULL) && (Aux1->Nombre != Nombre) && (Aux1->Telefono != Numero)){
-			Aux2 = Aux1;
-			Aux1 = Aux1->Sig;
-		}
-		Aux3 = Aux1;
-		Aux1 = Aux1->Sig;
-		Aux2->Sig = Aux1;
-		Aux1->Ant = Aux2;
-		free(Aux3);
-		printf ("Contacto Eliminado \n\n");
-	}
-	else {
-		printf ("Contacto No Encontrado \n");
-	}
-	return Agenda;
-}
 void Buscar_Contacto(struct contacto *Agenda, char *Nombre, char *Numero){
 	//Punteros de Ayuda
 	struct contacto *Aux1 = NULL;
@@ -153,11 +129,6 @@ int main(){
 	Agenda = Agregar_Contacto(Agenda, "Alan", "121212121");
 	Agenda = Agregar_Contacto(Agenda, "Fernanda", "3232323232");	
 	Mostrar_Contacto(Agenda);
-	Agenda = Eliminar_Contacto_medio(Agenda, "Elias", "232323232" );
-	Mostrar_Contacto(Agenda);
-	Agenda = Agregar_Contacto(Agenda, "Elias", "232323232");
-	Mostrar_Contacto(Agenda);
-	Buscar_Contacto(Agenda, "Fany", "212121212");
 
   
 return 0;
